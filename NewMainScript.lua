2af7cf6099462286f6064ab901d5aaab391ee94b
randombackup1293 <themagicpiston145@gmail.com>
2024-12-08 01:59:06 -0500

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VoidwareBakup/main/NewMainScript.lua", true))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/randombackup1293/PastewareModded/main/NewMainScript.lua", true))()
